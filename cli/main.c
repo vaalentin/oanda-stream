e79b0eb8775339fc86a3cf6d1415ae97bc09864c
Vaalentin <vaalentin@protonmail.com>
2016-03-05 07:38:00 +0100

    add curl request

--- cli/main.c
+++ cli/main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
+#include <curl/curl.h>
 
 #include "db.h"
 #include "query.h"
@@ -8,6 +10,11 @@
 #include "utils.h"
 
 sqlite3* db = NULL;
+
+size_t handle_chunk(void* buffer, size_t size, size_t nmemb, void* userp) {
+  printf("%s\n", (char*) buffer);
+  return size * nmemb;
+}
 
 int main(int argc, char* argv[]) {
   // config
@@ -47,7 +54,27 @@ int main(int argc, char* argv[]) {
   char* url = query_get_url(config.id, instrs_query);
   char* auth_header = query_get_auth_header(config.tok);
 
+  // query
+  CURL* handle = curl_easy_init();
+
+  struct curl_slist* headers = NULL;
+  headers = curl_slist_append(headers, auth_header);
+
+  curl_easy_setopt(handle, CURLOPT_URL, url);
+  curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
+  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, handle_chunk);
+  curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
+
+  CURLcode res = curl_easy_perform(handle);
+
+  if(res != CURLE_OK) {
+    printf("%s\n", curl_easy_strerror(res));
+    exit(EXIT_FAILURE);
+  }
+
   // clean
+  free(headers);
+  curl_easy_cleanup(handle);
   free(instrs_query);
   free(url);
   free(auth_header);
